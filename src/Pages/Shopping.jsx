fc5f0afc171cf0258e749a1fedce2e4c62c5d8df
Bonnie Lee <leebonniek@gmail.com>
2023-09-11 18:04:27 -0700

    added 5 pages on router-route-dom and deploys and routes pages

---
+++ src/Pages/Shopping.jsx
@@ -0,0 +1,7 @@
+export function Shopping(){
+    return <>
+      <div  >
+         <h1> Shopping </h1>
+      </div>
+    </>
+  }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import {Shopping} from "./Pages/Shopping";
+import {Login} from "./Pages/Login";
+import {Register} from "./Pages/Register";
+import {Cart} from "./Pages/Cart";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes> 
             <Route path="/home" element={<App/>} />
-{/*         <Route path="/shopping" element={<Shopping/>} />
+            <Route path="/shopping" element={<Shopping/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
             <Route path="/cart" element={<Cart/>} />
-*/}
+
         </Routes>
         
     
